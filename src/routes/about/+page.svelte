aed6ed59aa846dadbab1e159f846a140e2946a16
Brad Dougherty <me@brad.is>
2022-08-15 19:30:56 -0400

    Update about page text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,7 +6,7 @@
 
 <p>A Secret Space Agency program for checking the fucking tide.</p>
 <p>
-	Powered by the <a href="https://tides.app">tides.app API</a> and inspired by
+	Powered by the <a href="https://tides.app">Tides.app</a> and inspired by
 	<a href="http://thefuckingweather.com" target="_blank" rel="noopener">The Fucking Weather</a>.
 </p>
 <p><a href="/">Back to the fucking tide</a>.</p>
